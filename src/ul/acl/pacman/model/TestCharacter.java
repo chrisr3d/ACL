06350c87c8d02269fe377727cb6e4239db1767ba
lesquoyb <nouvelleaddresse@yahoo.fr>
2015-11-12 20:09:42 +0100

    yolo

---
+++ src/ul/acl/pacman/model/TestCharacter.java
@@ -0,0 +1,17 @@
+package ul.acl.pacman.model;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+
+public class TestCharacter {
+
+
+    @Test
+    public void voila(){
+        assertTrue(true);
+    }
+
+
+}
