0c73f4e28c8c0bbc7b9f21c087e982b2cac32bdb
lesquoyb <nouvelleaddresse@yahoo.fr>
2015-11-12 11:50:02 +0100

    controllerListener

--- src/ul/acl/pacman/model/Hero.java
+++ src/ul/acl/pacman/model/Hero.java
@@ -1,13 +1,20 @@
 package ul.acl.pacman.model;
 
+import ul.acl.pacman.controller.ControllerListener;
+
 /**
  * Created by baptiste on 11/11/15.
  */
 public class Hero extends Playable{
 
+    ControllerListener l;
     public Hero(int x, int y){
         super(x, y);
+        l = new ControllerListener();
+
     }
+
+
 
 
 
